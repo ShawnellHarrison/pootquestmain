22e71ef76b96f2c5066a8f81c33f8f70ac0336a6
Shawnell Harrison <shawnelldh@gmail.com>
2025-10-30 21:34:53 +0000

    any bugs, fix without causing other bugs

--- .next/static/chunks/src_app_adventure_[class]_page_tsx_b612b4e9._.js
+++ .next/static/chunks/src_app_adventure_[class]_page_tsx_b612b4e9._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_8b5ce8f6._.js",
+  "static/chunks/src_ea267d79._.js",
   "static/chunks/node_modules_next_e0f8e000._.js",
   "static/chunks/node_modules_@dnd-kit_core_dist_core_esm_deb93fb9.js",
   "static/chunks/node_modules_lodash_f240f67a._.js",
