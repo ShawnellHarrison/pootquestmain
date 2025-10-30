07d7932c91d53771b28e337e60fdfab58b904dfe
Shawnell Harrison <shawnelldh@gmail.com>
2025-10-30 21:27:02 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Missing or insuffi

--- .next/static/chunks/src_app_adventure_[class]_page_tsx_b612b4e9._.js
+++ .next/static/chunks/src_app_adventure_[class]_page_tsx_b612b4e9._.js
@@ -1,11 +1,12 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_d0ed0281._.js",
+  "static/chunks/src_8b5ce8f6._.js",
+  "static/chunks/node_modules_next_e0f8e000._.js",
   "static/chunks/node_modules_@dnd-kit_core_dist_core_esm_deb93fb9.js",
   "static/chunks/node_modules_lodash_f240f67a._.js",
   "static/chunks/node_modules_recharts_es6_f5341134._.js",
-  "static/chunks/node_modules_d870a958._.js"
+  "static/chunks/node_modules_917e63d5._.js"
 ],
     source: "dynamic"
 });
